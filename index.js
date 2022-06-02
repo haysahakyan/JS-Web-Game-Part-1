301df6048b6e8f96390c3b87b51522875f82e74f
haysahakyan <sahakyanhayarpi0@gmail.com>
2022-06-01 20:53:44 -0700

    done-part-1

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+document.body.append(object)
+return object
+}
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        
+        object.remove()
+
+    })
+}
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
+
+// let greenCharacter = document.createElement('img')
+// greenCharacter.src = 'assets/green-character.gif'
+// greenCharacter.style.position = 'fixed'
+// greenCharacter.style.left = '100px'
+// greenCharacter.style.bottom = '100px'
+// document.body.append(greenCharacter)
+
+
+//2. Add a Pine Tree
+// let tree = document.createElement('img')
+// tree.src = 'assets/pine-tree.png'
+// tree.style.position = 'fixed'
+// tree.style.left = '450px'
+// tree.style.bottom = '200px'
+// document.body.append(tree)
+
+//3. Refactor Using a Function
+
