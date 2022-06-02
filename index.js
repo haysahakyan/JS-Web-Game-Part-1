f35c260294e0464b25c4362a38806ab2eb959ed4
haysahakyan <sahakyanhayarpi0@gmail.com>
2022-06-01 20:59:06 -0700

    done-part-1-withBonus

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -17,6 +26,14 @@ function newItem(url, left, bottom){
 
     })
 }
+
+
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
 
 
 newImage('assets/green-character.gif', 100, 100)
